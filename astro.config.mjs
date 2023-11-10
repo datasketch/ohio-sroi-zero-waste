eb771326df2bd28c9a974bf8ce2316846ec291ca
David Daza <03dazal@gmail.com>
2023-11-10 08:55:52 -0500

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), sitemap()],
+  site: 'https://lucent-bunny-643535.netlify.app',
+  build: {
+    assetsPrefix: 'https://lucent-bunny-643535.netlify.app'
+  }
 });
