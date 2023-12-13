87b52f3916851325980bf3d5ce8315f2093e65d7
David Daza <03dazal@gmail.com>
2023-12-13 08:53:22 -0500

    Update build

---
+++ dist/_astro/Header_logo.a3873295.js
@@ -0,0 +1,1 @@
+import{j as a}from"./jsx-runtime.4844db2a.js";import"./index.453e6029.js";await fetch("./data/format.json").then(t=>t.json());function r(){return a.jsx("div",{className:"u-container"})}export{r as default};
