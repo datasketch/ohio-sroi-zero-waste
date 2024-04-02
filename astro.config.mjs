7db9208709be2d11baa394f00525dd0e58859aec
David Daza <03dazal@gmail.com>
2024-04-02 09:21:10 -0500

    Update outDir

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [tailwind(), react()],
   site: 'https://ousroi.datasketch.co/zero-waste',
+  base: '/zero-waste',
+  outDir: './dist/zero-waste',
   build: {
     assetsPrefix: process.env.PREFIX_ASSETS === 'yes' ? 'https://ousroi.datasketch.co/zero-waste' : ''
   }
